4802519e02a51c2bf818ff0f8e2470c43ef37a6a
Rakesh Vidyadharan <rakesh@sptci.com>
2024-12-27 13:47:22 -0600

    Add example

---
+++ example/server.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <nghttp2/asio_http2_server.h>
+
+using namespace nghttp2::asio_http2;
+using namespace nghttp2::asio_http2::server;
+
+int main()
+{
+  boost::system::error_code ec;
+  http2 server;
+
+  server.handle("/", [](const request&, const response& res)
+  {
+    res.write_head(200);
+    res.end("hello, world\n");
+  });
+
+  std::cout << "Starting HTTP/2 server on localhost:3000\n";
+  std::cout << "Test with curl --http2-prior-knowledge http://localhost:3000\n";
+  if (server.listen_and_serve(ec, "localhost", "3000"), true)
+  {
+    std::cerr << "error: " << ec.message() << std::endl;
+  }
+}
